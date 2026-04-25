77c81a718367f4907518bce0a3d797c8214c09b7
jayabharathi-s20 <jayabharathis@bitcot.com>
2026-04-25 11:58:31 +0530

    week-2 day-2 modified

--- Week-2/Day-2/crud.py
+++ Week-2/Day-2/crud.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from models import User
 
 def create_user(db: Session, name: str, age: int):
+    """Create a new user"""
     user = User(name=name, age=age)
     db.add(user)
     db.commit()
@@ -9,13 +10,16 @@ def create_user(db: Session, name: str, age: int):
     return user
 
 def get_all_users(db: Session):
+    """Get all the users"""
     return db.query(User).all()
 
 
 def get_user(db: Session, user_id: int):
+    """Getting single user with id"""
     return db.query(User).filter(User.id == user_id).first()
 
 def update_full_user(db: Session, user_id: int, name: str, age: int):
+    """update all the users --method(put)"""
     user = get_user(db, user_id)
     if user:
         user.name = name
@@ -25,6 +29,7 @@ def update_full_user(db: Session, user_id: int, name: str, age: int):
     return user
 
 def update_partial_user(db: Session, user_id: int, name: str, age: int):
+    """update specific field of the users --method(patch)"""
     user = get_user(db, user_id)
     if user:
         if name is not None:
@@ -37,6 +42,7 @@ def update_partial_user(db: Session, user_id: int, name: str, age: int):
 
 
 def delete_user(db: Session, user_id: int):
+    """delete particular user with ref to id"""
     user = get_user(db, user_id)
     if user:
         db.delete(user)
--- Week-2/Day-2/database.py
+++ Week-2/Day-2/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy.orm import sessionmaker,declarative_base
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-database_url="postgresql://postgres:password@localhost:5432/mydb"
+load_dotenv()
+db_user=os.getenv("db_user")
+db_pw=os.getenv("db_pw")
+db_host=os.getenv("db_host")
+db_port=os.getenv("db_port")
+db_name=os.getenv("db_name")
+
+database_url=f"postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}"
 engine=create_engine(database_url)
 
 session_creation=sessionmaker(autoflush=False,autocommit=False,bind=engine)
 Base=declarative_base()
+
